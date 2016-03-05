6c26e2a51eed00b6e8a9777561024ca0e5e8e102
Andres Urquijo <andyurquijo@gmail.com>
2016-03-04 23:29:29 -0500

    Improved visuals. Refactored folders for intern assets. Avatars added to pool.

---
+++ Assets/AvatarCreation/Scripts/LookAt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LookAt : MonoBehaviour 
+{
+    public Transform target;
+    public bool ignoreY;
+
+	void Update () 
+	{
+        if (!target)
+            return;
+
+        Vector3 targetPos = target.position;
+
+        if (ignoreY)
+            targetPos.y = transform.position.y;
+
+        transform.LookAt(targetPos);
+	}
+}
